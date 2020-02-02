71e83fe422cd2bc4d441e0cd258eb0a459376023
Deepika Onteru <deepikaonteru@Deepikas-MacBook-Pro.local>
2020-02-01 19:25:09 -0500

    header bar

---
+++ connect-app/src/Headerbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Headerbar extends React.Component{
+
+    render() {
+        return (
+            <div className="header">
+                <h1>Title</h1>
+            </div>
+        );
+    }
+}
+
+export default Headerbar;
---
+++ connect-app/src/WelcomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Headerbar from './Headerbar.js';
+
+class WelcomePage extends React.Component {
+
+    render() {
+        return (
+            <Headerbar> </Headerbar>
+        );
+    }
+
+}
+
+export default WelcomePage;
